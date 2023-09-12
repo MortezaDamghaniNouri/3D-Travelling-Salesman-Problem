469fdba8717d8e6ed87d3f47def41e65f7541a9f
Morteza Damghani <mortezadamghani77@gmail.com>
2023-09-12 15:58:22 -0400

    random_chromosome_generator function is implemented

--- main.py
+++ main.py
@@ -1,3 +1,38 @@
+import random
+
+# This function generates some random chromosomes. The size of the chromosome is given as an input
+def random_chromosome_generator(size):
+    output_list = []
+    k = 0
+    while k < size:
+        random_number = random.randint(0, (size - 1))
+        if len(output_list) == 0:
+            output_list.append(random_number)
+        else:
+            if random_number not in output_list:
+                output_list.append(random_number)
+            else:
+                while True:
+                    new_random_number = random.randint(0, (size - 1))
+                    if new_random_number not in output_list:
+                        output_list.append(new_random_number)
+                        break
+        k += 1
+    return output_list
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 # reading from the input file
@@ -23,10 +58,41 @@ while i < len(lines):
     cities.append(temp_list)
     i += 1
 
-print("the len of lines: " + str(len(lines)))
-print("number of cities: " + str(number_of_cities))
-for i in cities:
-    print(i)
+# generating the first generation
+number_of_population = 1000
+population = []
+i = 1
+
+j = 0
+while j < 6:
+    print(random_chromosome_generator(5))
+    j += 1
+
+
+
+# while i <= number_of_population:
+#     temp_list = [random_chromosome_generator(number_of_cities)]
+#     temp_list.append(fitness_calculator(temp_list[0]))
+#
+#
+#
+#     i += 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
