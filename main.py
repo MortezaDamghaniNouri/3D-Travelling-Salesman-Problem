2f60701783dbd3b318bdfa70e76b08ee187bc8e2
Morteza Damghani <mortezadamghani77@gmail.com>
2023-09-12 14:59:36 -0400

    Reading the input part is done and tested

--- main.py
+++ main.py
@@ -7,20 +7,24 @@ while True:
     line = input_file.readline()
     if line == "":
         break
-    lines.append(line)
+    lines.append(line.rstrip())
 input_file.close()
 
 # parsing the lines of the input file
 cities = []
-number_of_cities = lines[0]
+number_of_cities = int(lines[0])
 i = 1
 while i < len(lines):
     temp_list = lines[i].split(" ")
+    j = 0
+    while j < len(temp_list):
+        temp_list[j] = int(temp_list[j])
+        j += 1
     cities.append(temp_list)
     i += 1
 
-
-print(number_of_cities)
+print("the len of lines: " + str(len(lines)))
+print("number of cities: " + str(number_of_cities))
 for i in cities:
     print(i)
 
