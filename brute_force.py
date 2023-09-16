081e944cf4d941fc6c603163e3a0b04974b15b63
Morteza Damghani <mortezadamghani77@gmail.com>
2023-09-16 14:04:38 -0400

    brute_force file is implemented

--- brute_force.py
+++ brute_force.py
@@ -6,16 +6,44 @@ import math
 
 def list_of_numbers_generator(input_number):
     output_list = []
-    i = 1
-    while i <= input_number:
+    i = 0
+    while i < input_number:
         output_list.append(i)
         i += 1
     return output_list
 
 
+# This function returns the euclidean distance between the two input cities
+def euclidean_distance_calculator(city_one, city_two):
+    return math.sqrt(pow((city_two[0] - city_one[0]), 2) + pow((city_two[1] - city_one[1]), 2) + pow((city_two[2] - city_one[2]), 2))
 
 
+# This function calculates the fitness of the input chromosomes
+def total_distance_calculator(input_chromosome, list_of_cities):
+    fitness = 0
+    k = 0
+    while k < (len(input_chromosome) - 1):
+        fitness = fitness + euclidean_distance_calculator(list_of_cities[input_chromosome[k]], list_of_cities[input_chromosome[k + 1]])
+        k += 1
+    fitness = fitness + euclidean_distance_calculator(list_of_cities[input_chromosome[0]], list_of_cities[input_chromosome[len(input_chromosome) - 1]])
+    return fitness
 
+
+# This function sorts the input list
+def sort(input_list):
+    k = len(input_list) - 2
+    while k >= 0:
+        j = 0
+        while j <= k:
+            if input_list[j][1] > input_list[j + 1][1]:
+                temp = input_list[j]
+                input_list[j] = input_list[j + 1]
+                input_list[j + 1] = temp
+            j += 1
+
+        k = k - 1
+
+    return input_list
 
 
 
@@ -70,26 +98,35 @@ while i <= math.factorial(number_of_cities):
         if result not in results:
             results.append(result)
             break
-    print(i)
+
+    if i % 100 == 0:
+        print(i)
     i += 1
 
-print("the len of results: " + str(len(results)))
-print(results)
+print("The results list is generated")
+print("The len of results: " + str(len(results)))
+# print(results)
+
+all_possible_chromosomes = []
+i = 0
+while i < len(results):
+    all_possible_chromosomes.append([results[i], total_distance_calculator(results[i], cities)])
+    i += 1
+
+print("Sorting all_possible_chromosomes...")
+all_possible_chromosomes = sort(all_possible_chromosomes)
+
+output_file = open("bf_output.txt", "wt")
+
+i = 0
+while i < len(all_possible_chromosomes):
+
+    output_file.write(str(round(all_possible_chromosomes[i][1], 5)) + ": " + str(all_possible_chromosomes[i][0].append(all_possible_chromosomes[i][0][0])) + "\n")
+    i += 1
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
+output_file.close()
 
 
 
